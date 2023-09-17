2ff34f100a4921f2a8c8188dc214e59702c44140
JuniorM2702 <Junior_aybar27@hotmail.com>
2023-09-17 13:38:12 -0500

    first commit try

---
+++ src/main/java/edu/pe/cibertec/githubsemana04/GithubSemana04Application.java
@@ -0,0 +1,13 @@
+package edu.pe.cibertec.githubsemana04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubSemana04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubSemana04Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/pe/cibertec/githubsemana04/GithubSemana04ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pe.cibertec.githubsemana04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubSemana04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
