eba1ec9d0f08000dffb705572e71d3ec7ca3fa22
JuniorM2702 <Junior_aybar27@hotmail.com>
2023-09-17 17:01:16 -0500

    desarrollo del ejercicio1

--- src/main/java/edu/pe/cibertec/githubsemana04/model/PromedioModel.java
+++ src/main/java/edu/pe/cibertec/githubsemana04/model/PromedioModel.java
@@ -1,4 +1,19 @@
 package edu.pe.cibertec.githubsemana04.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PromedioModel {
+    private Integer cl1;
+    private Integer cl2;
+    private Integer cl3;
+    private Integer profin;
+
+
 }
